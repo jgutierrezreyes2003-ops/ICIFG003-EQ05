b35ce614f144ed44f2a643edc5a5a085d97996bf
jgutierrezreyes2003-ops <jgutierrezreyes2003@gmail.com>
2026-05-05 10:23:35 -0400

    landing page, login, inicio funcional

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/usuarios/pages/home/home.component';
 import { UsuarioFormComponent } from './features/usuarios/components/usuario/usuario-form.component'; 
 import { InicioComponent } from './features/usuarios/components/inicio/inicio.component';
 
 export const routes: Routes = [
-
-  { path: '', component: UsuarioFormComponent }, 
-  { path: 'inicio', component: InicioComponent },  
+  { path: '', component: HomeComponent }, 
+  { path: 'login', component: UsuarioFormComponent },
+  { path: 'inicio', component: InicioComponent },
   { path: '**', redirectTo: '' }
 ];
